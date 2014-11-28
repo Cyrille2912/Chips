5c542079f57bef6c87c5cacfd855822b8eee731c
Ader0bgaltis <george.goultidis@gmail.com>
2014-11-28 14:08:20 +0100

    file where the algorithms will be

---
+++ algorithms.py
@@ -0,0 +1,20 @@
+
+
+
+def random_constraint_relaxation_algorithm():
+    return [((0,0),(1000,1000),0)]
+
+def random_algorithm():
+    return [((0,0),(1000,2000),0)]
+
+def a_star_algorithm():
+    return [((0,0),(1000,3000),0)]
+
+
+
+
+
+
+
+if __name__ is "__main__":
+    pass
